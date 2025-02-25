ad838c377296245b900767d835f982f5ee76a6fa
lucasfrancobn <lucasfbnavarro@gmail.com>
2025-02-25 19:50:19 -0300

    feat: criando projeto

---
+++ src/main/java/io/github/lucasfrancobn/gamemaster/GamemasterApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasfrancobn.gamemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamemasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/lucasfrancobn/gamemaster/GamemasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasfrancobn.gamemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
